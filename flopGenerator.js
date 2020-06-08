fd34fde7c7340cdddff4d82fabd422a4ee4a3cd6
Ibrahim T. Miraj <zerobottles@outlook.com>
2020-06-08 03:35:59 -0400

    first commit

---
+++ flopGenerator.js
@@ -0,0 +1,9 @@
+class flopGenerator {
+    convertor = new Convertor();
+    num2card = convertor.num2card;
+    card2num = convertor.card2num;
+
+    generateInsideStraight(hole1, hole2) {
+        
+    }
+}
