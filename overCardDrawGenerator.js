05195623a27428541324ccffa4ff07614e6e8b13
Ibrahim T. Miraj <zerobottles@outlook.com>
2020-06-15 13:20:36 -0400

    faulty depopulateUntil2Elements method

--- overCardDrawGenerator.js
+++ overCardDrawGenerator.js
@@ -1,5 +1,5 @@
-let utilities = require('./utilities.js');
-let convertor = require('./convertor.js');
+let Utilities = require('./utilities.js');
+let Convertor = require('./convertor.js');
 
 //Having an Ace in the input might fuck things up using a 
 //Low ace scheme, as A = 1 will be low, so the second
@@ -9,6 +9,7 @@ let convertor = require('./convertor.js');
 //Having an A automatically makes it use high card scheme
 
 function decideConversionScheme(hole1, hole2) {
+    
     let convertor = Convertor.putCardGetNumAceLow;
     let backConvertor = Convertor.putNumGetCardValueAceLow;
 
@@ -40,7 +41,7 @@ function populateUnderNumArr(holeConverted, holeUnderNumArr) {
             holeUnderNumArr.push(a);
         }
     }
-    console.log(holeUnderNumArr);
+    //console.log(holeUnderNumArr);
 }
 
 function populateOverNumArr(holeConverted, holeOverNumArr) {
@@ -64,15 +65,111 @@ function populateAcrossNumArr(holeConverted, holeAcrossNumArr) {
 }
 
 
+
+//THIS DOESN'T WORK, ESSENTIALLY YOU WANT IT SO THAT WHENEVER THIS IS CALLED,
+//IT REMOVES THE NUMBER FROM THE PRIMARY ARRAY ASWELL AS ALL SECONDARY ARRAYS,
+//BUT KEEPS THE NUMBER OF ELEMENTS IN EACH ARRAY ABOVE 2 WHENEVER POSSIBLE
+//CURRENTLY, IT LEAVES NUMBERS IT HAS REMOVED FROM ONE ARRAY IN OTHER ARRAYS, POSSIBLY DUE TO THE >2 restriction saving numbers that should have been removed
+function depopulateUntil2Elements(removedNumberArr, primaryArray, secondaryArray, secondaryArray2, secondaryArray3, secondaryArray4, secondaryArray5) {
+
+    let firstRemovedElement;
+    let secondRemovedElement;
+
+    //Randomly remove 2 numbers from hole1AcrossNumArr
+    if(primaryArray.length > 2) {
+        let firstRemovedElement = primaryArray.splice(Math.floor(Math.random() * primaryArray.length), 1);
+        console.log('Removing element: ' + firstRemovedElement)
+        removedNumberArr.push(firstRemovedElement);
+        
+    }
+
+    if(primaryArray.length > 2) {
+        let secondRemovedElement = primaryArray.splice(Math.floor(Math.random() * primaryArray.length), 1);
+        console.log('Removing element: ' + secondRemovedElement)
+        removedNumberArr.push(secondRemovedElement);
+    }
+
+    //Remove numbers you removed from primaryArray, until length is 2
+    if(secondaryArray2.length > 2 && firstRemovedElement) {
+        secondaryArray2.splice(secondaryArray2.indexOf(firstRemovedElement), 1);
+        console.log('Removing element: ' + firstRemovedElement)
+        console.log('In secondaryArray2 1')
+    }
+
+    if(secondaryArray2.length > 2 && secondRemovedElement) {
+        secondaryArray2.splice(secondaryArray2.indexOf(secondRemovedElement), 1);
+        console.log('Removing element: ' + secondRemovedElement)
+        console.log('In secondaryArray2 2')
+    }
+
+    //Remove numbers you removed from primaryArray, until length is 2
+    if(secondaryArray.length > 2 && firstRemovedElement) {
+        secondaryArray.splice(secondaryArray.indexOf(firstRemovedElement), 1);
+        console.log('Removing element: ' + firstRemovedElement)
+        console.log('In secondaryArray 1')
+    }
+
+    if(secondaryArray.length > 2 && secondRemovedElement) {
+        secondaryArray.splice(secondaryArray.indexOf(secondRemovedElement), 1);
+        console.log('Removing element: ' + secondRemovedElement)
+        console.log('In secondaryArray 2')
+    }
+
+    //Remove numbers you removed from primaryArray, until length is 2
+    if(secondaryArray3.length > 2 && firstRemovedElement) {
+        secondaryArray3.splice(secondaryArray3.indexOf(firstRemovedElement), 1);
+        console.log('Removing element: ' + firstRemovedElement)
+        console.log('In secondaryArray3 1')
+    }
+
+    if(secondaryArray3.length > 2 && secondRemovedElement) {
+        secondaryArray3.splice(secondaryArray3.indexOf(secondRemovedElement), 1);
+        console.log('Removing element: ' + secondRemovedElement)
+        console.log('In secondaryArray3 2')
+    }
+
+    //Remove numbers you removed from primaryArray, until length is 2
+    if(secondaryArray5.length > 2 && firstRemovedElement) {
+        secondaryArray5.splice(secondaryArray5.indexOf(firstRemovedElement), 1);
+        console.log('Removing element: ' + firstRemovedElement)
+        console.log('In secondaryArray5 1')
+    }
+
+    if(secondaryArray5.length > 2 && secondRemovedElement) {
+        secondaryArray5.splice(secondaryArray5.indexOf(secondRemovedElement), 1);
+        console.log('Removing element: ' + secondRemovedElement)
+        console.log('In secondaryArray5 2')
+    }
+
+    //Remove numbers you removed from primaryArray, until length is 2
+    if(secondaryArray4.length > 2 && firstRemovedElement) {
+        secondaryArray4.splice(secondaryArray4.indexOf(firstRemovedElement), 1);
+        console.log('Removing element: ' + firstRemovedElement)
+        console.log('In secondaryArray4 1')
+    }
+
+    if(secondaryArray4.length > 2 && secondRemovedElement) {
+        secondaryArray4.splice(secondaryArray4.indexOf(secondRemovedElement), 1);
+        console.log('Removing element: ' + secondRemovedElement)
+        console.log('In secondaryArray4 2')
+    }
+
+    console.log('method end' )
+}
+
+
 function generateOneOvercardDraw(hole1, hole2) {
     let flopArr;
     let availableNumberArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-    let convertor = convertor.putCardGetNumAceHigh;
-    let backConvertor = convertor.putNumGetCardValueAceHigh;
+    let removedNumberArr = []
+    let convertor = Convertor.putCardGetNumAceHigh;
+    let backConvertor = Convertor.putNumGetCardValueAceHigh;
     hole1UnderNumArr = [];
     hole1OverNumArr = [];
     hole1AcrossNumArr = [];
     hole2UnderNumArr = [];
+    hole2OverNumArr = [];
+    hole2AcrossNumArr = [];
 
     //Converts cards using decided conversion scheme
     hole1Converted = convertor[hole1];
@@ -85,59 +182,71 @@ function generateOneOvercardDraw(hole1, hole2) {
         hole2Converted = temp;
       }  
 
-    if(hole2Converted === hole2Converted ) {
-        console.log("This function is not meant for pocket pairs")
-        return;
-    } else if (hole2Converted < 5) {
-        console.log("One Overcard Draw not possible when your highest hole card is less than 5");
-        return;
-    }
+    // if(hole2Converted === hole2Converted ) {
+    //     console.log("This function is not meant for pocket pairs")
+    //     return;
+    // } else if (hole2Converted < 5) {
+    //     console.log("One Overcard Draw not possible when your highest hole card is less than 5");
+    //     return;
+    // }
 
     //Just remove numbers corresponding to hole cards from available pool
-    remainingNumberArray.splice(remainingNumberArray.indexOf(hole1Converted), 1);
-    remainingNumberArray.splice(remainingNumberArray.indexOf(hole2Converted), 1);
+    availableNumberArr.splice(availableNumberArr.indexOf(hole1Converted), 1);
+    availableNumberArr.splice(availableNumberArr.indexOf(hole2Converted), 1);
 
-    //Remove numbers higher than intended overcard
-    remainingNumberArray = remainingNumberArray.filter((number) => {
-        return number < hole2Converted;
-    })
+    // //Remove numbers higher than intended overcard
+    // remainingNumberArray = remainingNumberArray.filter((number) => {
+    //     return number < hole2Converted;
+    // })
 
     //Implement straight generation protection
 
+    console.log(`hole1: ${hole1Converted}`);
+    console.log(`hole2: ${hole2Converted}`);
+
     //Populate hole1 arrays
-    populateUnderNumArr(hole1, hole1UnderNumArr);
-    populateOverNumArr(hole1, hole1OverNumArr);
-    populateAcrossNumArr(hole1, hole1AcrossNumArr);
+    populateUnderNumArr(hole1Converted, hole1UnderNumArr);
+    populateOverNumArr(hole1Converted, hole1OverNumArr);
+    populateAcrossNumArr(hole1Converted, hole1AcrossNumArr);
+    console.log(`hole1UnderNumArr: ${hole1UnderNumArr}`);
+    console.log(`hole1OverNumArr: ${hole1OverNumArr}`);
+    console.log(`hole1AcrossNumArr: ${hole1AcrossNumArr}`);
 
     //Populate hole2 arrays
-    //No use populating over and across arrays, as they have already been depopulated
-    //sufficiently by removing all numbers above hole2Converted
-    populateUnderNumArr(hole2, hole2UnderNumArr);
+    populateUnderNumArr(hole2Converted, hole2UnderNumArr);
+    populateOverNumArr(hole2Converted, hole2OverNumArr);
+    populateAcrossNumArr(hole2Converted, hole2AcrossNumArr);
+    console.log(`hole2UnderNumArr: ${hole2UnderNumArr}`);
+    console.log(`hole2OverNumArr: ${hole2OverNumArr}`);
+    console.log(`hole2AcrossNumArr: ${hole2AcrossNumArr}`);
 
-    //Randomly remove 2 numbers from hole1AcrossNumArr
-    let hole1RemovedNum1 = hole1AcrossNumArr.splice(Math.floor(Math.random() * remainingNumberSet.length));
-    let hole1RemovedNum2 = hole1AcrossNumArr.splice(Math.floor(Math.random() * remainingNumberSet.length));
 
-    //Remove numbers you removed from hole1AcrossNumArr from hole1OverNumArr
-    hole1OverNumArr.splice(hole1OverNumArr.indexOf(hole1RemovedNum1), 1);
-    hole1OverNumArr.splice(hole1OverNumArr.indexOf(hole1RemovedNum2), 1);
+    depopulateUntil2Elements(removedNumberArr, hole1AcrossNumArr, hole1UnderNumArr, hole1OverNumArr, hole2AcrossNumArr, hole2UnderNumArr, hole2OverNumArr);
+    depopulateUntil2Elements(removedNumberArr, hole2AcrossNumArr, hole1UnderNumArr, hole1OverNumArr, hole1AcrossNumArr, hole2UnderNumArr, hole2OverNumArr);
+    depopulateUntil2Elements(removedNumberArr, hole1UnderNumArr, hole1AcrossNumArr, hole1OverNumArr, hole2AcrossNumArr, hole2UnderNumArr, hole2OverNumArr);
+    depopulateUntil2Elements(removedNumberArr, hole1OverNumArr, hole1UnderNumArr, hole1AcrossNumArr, hole2AcrossNumArr, hole2UnderNumArr, hole2OverNumArr);
+    depopulateUntil2Elements(removedNumberArr, hole2UnderNumArr, hole1UnderNumArr, hole1OverNumArr, hole2AcrossNumArr, hole1AcrossNumArr, hole2OverNumArr);
+    depopulateUntil2Elements(removedNumberArr, hole2OverNumArr, hole1UnderNumArr, hole1OverNumArr, hole2AcrossNumArr, hole2UnderNumArr, hole1AcrossNumArr);
 
-    //Remove numbers you removed from hole1AcrossNumArr from hole1UnderNumArr
-    hole1UnderNumArr.splice(hole1UnderNumArr.indexOf(hole1RemovedNum1), 1);
-    hole1UnderNumArr.splice(hole1UnderNumArr.indexOf(hole1RemovedNum2), 1);
+    console.log(`removedNumberArr: ${removedNumberArr}`);
+    console.log(`hole1UnderNumArr: ${hole1UnderNumArr}`);
+    console.log(`hole1OverNumArr: ${hole1OverNumArr}`);
+    console.log(`hole1AcrossNumArr: ${hole1AcrossNumArr}`);
+    console.log(`hole2UnderNumArr: ${hole2UnderNumArr}`);
+    console.log(`hole2OverNumArr: ${hole2OverNumArr}`);
+    console.log(`hole2AcrossNumArr: ${hole2AcrossNumArr}`);
 
-    //Remove numbers you removed from hole1AcrossNumArr from hole1OverNumArr
-    hole1OverNumArr.splice(hole1OverNumArr.indexOf(hole1RemovedNum1), 1);
-    hole1OverNumArr.splice(hole1OverNumArr.indexOf(hole1RemovedNum2), 1);
-
-    while(hole1Under)
+    
+    // console.log(hole1AcrossNumArr.length);
+    // console.log(hole1UnderNumArr.length);
+    // console.log(hole1OverNumArr.length);
+    // console.log(hole2AcrossNumArr.length)
+    // console.log(hole2UnderNumArr.length);
+    // console.log(hole2OverNumArr.length);
 
     //Then, just implement loop to regenerate suit values if it's a flush draw
 
 
 }
 
-let holeAcrossNumArr = []
-
-populateAcrossNumArr(6, holeAcrossNumArr);
-console.log(holeAcrossNumArr);
+generateOneOvercardDraw('Ac', 'Kd');
